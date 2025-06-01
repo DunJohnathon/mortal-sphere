a727fcc9219338e3c107a8f26a84610f739fe106
DunJohnathon <johndavidwaskow@gmail.com>
2025-06-01 13:29:26 -0500

    Update leaflet-maps.js

--- src/assets/leaflet-maps.js
+++ src/assets/leaflet-maps.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       mapDiv.style = "width: 100%; height: 400px; margin: 1em 0;";
       codeBlock.parentElement.replaceWith(mapDiv);
 
-//      const map = L.map(mapId).setView(
+      const map = L.map(mapId)
 //        [config.latitude, config.longitude],
 //        config.zoom || 10
 //      );
