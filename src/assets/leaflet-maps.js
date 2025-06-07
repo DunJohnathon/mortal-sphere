dd9fd9a26c68952260fae886b6ed3374c18cac38
DunJohnathon <johndavidwaskow@gmail.com>
2025-06-07 09:53:58 -0500

    Update leaflet-maps.js

--- src/assets/leaflet-maps.js
+++ src/assets/leaflet-maps.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 .replace(/[^\w\s-]/g, "")
                 .replace(/\s+/g, "-");
 
-              const url = findNoteUrlBySlug(slug)
+              const url = findNoteUrlBySlug(slug).toString()
               
               L.marker([marker.loc[0], marker.loc[1]],
                        {icon: icon}, {title: marker.link})
