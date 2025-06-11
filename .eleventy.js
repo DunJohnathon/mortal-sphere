a0e9316c11df92d3ad58511b54676c93cd612eee
DunJohnathon <johndavidwaskow@gmail.com>
2025-06-11 17:31:37 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -94,9 +94,9 @@ function getAnchorAttributes(filePath, linkTitle) {
 const tagRegex = /(^|\s|\>)(#[^\s!@#$%^&*()=+\.,\[{\]};:'"?><]+)(?!([^<]*>))/g;
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ 
-    "src/assets": "assets" 
-  });
+//  eleventyConfig.addPassthroughCopy({ 
+//    "src/assets": "assets" 
+//  });
   
   eleventyConfig.setLiquidOptions({
     dynamicPartials: true,
