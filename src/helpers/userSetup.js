961b5f0c75c4d818347deb03a7870e17c485f3a6
DunJohnathon <johndavidwaskow@gmail.com>
2025-06-11 17:32:01 -0500

    Update userSetup.js

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -5,6 +5,9 @@ function userMarkdownSetup(md) {
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+  eleventyConfig.addPassthroughCopy({ 
+    "src/assets": "assets" 
+  });
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
